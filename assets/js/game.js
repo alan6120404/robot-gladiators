8cb12da56cc92394f655ae083f3415bb377e19fc
Alan Lin <alin6120404@gmail.com>
2021-11-03 00:19:23 -0700

    slight tuning to shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -171,11 +171,11 @@ var endGame = function() {
 
 var shop = function() {
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
     )
 
     shopOptionPrompt = parseInt(shopOptionPrompt);
-    
+
     switch(shopOptionPrompt) {
         case 1:
             playerInfo.refillHealth();
